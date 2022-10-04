97b58542d5e2e31b63f64f2431edb073a5dd852f
Ivan <Korneychik91@yandex.ru>
2022-10-04 22:24:39 +0300

    examples_14 done

---
+++ examples_14/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введи целое число ");
+string usernum = Console.ReadLine() ?? "";
+int num = int.Parse(usernum);
+
+int a1 = num%7;
+int a2 = num%23;
+if (a1+a2==0)
+Console.WriteLine ("Ваше число " + num + ", одновременно кратно 7 и 23");
+else
+Console.Write ("Ваше число " + num + ", одновременно не кратно 7 и 23");
