d15a01a53320bdb0bb0ff914c29c7effd3db10a1
Ivan <Korneychik91@yandex.ru>
2022-10-04 22:47:58 +0300

    examples_16 done

---
+++ examples_16/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введи первое целое число ");
+string usernum1 = Console.ReadLine() ?? "";
+int num1 = int.Parse(usernum1);
+Console.Write("Введи второе целое число ");
+string usernum2 = Console.ReadLine() ?? "";
+int num2 = int.Parse(usernum2);
+
+if (num1%num2 == 0)
+Console.WriteLine(num1 +" является квадратом " + num2);
+else 
+if (num2%num1 == 0)
+Console.WriteLine(num2 +" является квадратом " + num1);
+else 
+Console.WriteLine(num2 +" неявляется квадратом " + num1 + " также как и " + num1 + " неявляется квадратом " + num2);
