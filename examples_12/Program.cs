59761b7cbd05a78d930065b21bfb8ff37105e686
Ivan <Korneychik91@yandex.ru>
2022-10-04 22:05:25 +0300

    examples_12 done

---
+++ examples_12/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введи первое целое число ");
+string usernum1 = Console.ReadLine() ?? "";
+int num1 = int.Parse(usernum1);
+Console.Write("Введи второе целое число ");
+string usernum2 = Console.ReadLine() ?? "";
+int num2 = int.Parse(usernum2);
+if (num2 > num1)
+{
+    if (num2 % num1 == 0)
+        Console.WriteLine(num2 + " кратно " + num1);
+    if (num2 % num1 != 0)
+        Console.WriteLine(num2 + " некратно " + num1 + ", остаток = " + num2 % num1);
+
+}
+else 
+{
+    Console.Write (num2 + " некратно " + num1);
+}
