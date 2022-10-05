bb268e5a05b3450ff2cc34e663d82b3d75179752
Ivan <Korneychik91@yandex.ru>
2022-10-06 00:11:32 +0300

    no

---
+++ HomeWork/examples_13/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что 3 цифры нет
+int usernum;
+
+while (true)
+{
+    Console.Write("Введи целое число ");
+    if (int.TryParse(Console.ReadLine(), out usernum))
+        break;
+    Console.WriteLine("Ошибка ввода, попробуй еще раз");
+}
+
+if (usernum / 10 < 10)
+{
+    if (usernum / 10 > -10)
+        Console.WriteLine($"в числе {usernum} нет третьего числа");
+}
+
+else
+{
+    int a = usernum;
+    if (a > 0)
+    {
+        while (a > 999)
+        {
+            a = a / 10;
+        }
+    }
+    if (a < 0)
+    {
+       while (a < -999)
+        {
+            a = a / 10;
+        } 
+    }
+
+    a = a % 10;
+    Console.Write($"в твоём числе {usernum} третья цифра это {a}");
+}
